3887fe6393d1c5bc45ae7f16e2f896ecbdf68367
dlwpg <2389038599@qq.com>
2020-09-07 13:55:46 +0800

    2020-09-07-测试连接

---
+++ graduationDesign/src/main/java/com/wpg/graduationdesign/GraduationdesignApplication.java
@@ -0,0 +1,13 @@
+package com.wpg.graduationdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationdesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationdesignApplication.class, args);
+    }
+
+}
